c8c7d0eaeb363fac34dd34d802a50c537f645570
Swayam Sandeep Wable <swayamwable3@gmail.com>
2026-07-11 00:57:23 +0530

    fixed CI: separate violation demo from compliant-infra gate check

---
+++ examples/compliant_infra/main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "customer_records" {
+  bucket = "customer-records-store"
+  region = "ap-south-1"
+  acl    = "private"
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    data_type = "financial"
+    purpose   = "customer-records"
+  }
+}
+
+resource "aws_db_instance" "payments_db" {
+  region              = "ap-south-1"
+  storage_encrypted   = true
+  publicly_accessible = false
+
+  tags = {
+    data_type = "payment"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "app_logs" {
+  name              = "app-logs"
+  retention_in_days = 180
+}
