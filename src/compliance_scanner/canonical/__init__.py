a41f7f9976a94fb30b7adb4a0ed114a6bd440924
Swayam Sandeep Wable <swayamwable3@gmail.com>
2026-08-17 21:07:03 +0530

    feat(canonical): add resource classifier

--- src/compliance_scanner/canonical/__init__.py
+++ src/compliance_scanner/canonical/__init__.py
@@ -1,5 +1,10 @@
 from .resource import CanonicalResource
+from .classifier import ResourceClassifier
+from .exceptions import CanonicalModelError, UnknownCanonicalResourceError
 
 __all__ = [
     "CanonicalResource",
+    "ResourceClassifier",
+    "CanonicalModelError",
+    "UnknownCanonicalResourceError",
 ]
