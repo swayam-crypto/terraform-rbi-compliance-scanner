3d07774687c251191fa190642192996136173d65
Swayam Sandeep Wable <swayamwable3@gmail.com>
2026-09-04 19:24:46 +0530

    refactor(engine): execute analysis engines through registry

--- src/compliance_scanner/engine/registry.py
+++ src/compliance_scanner/engine/registry.py
@@ -1,8 +1,9 @@
 from compliance_scanner.engine.attack.engine import AttackPathEngine
+from compliance_scanner.engine.base import AnalysisEngine
 from compliance_scanner.engine.blast_radius.engine import BlastRadiusEngine
 from compliance_scanner.engine.identity.engine import IdentityEngine
 
-ANALYSIS_ENGINES = (
+ANALYSIS_ENGINES: tuple[type[AnalysisEngine], ...] = (
     AttackPathEngine,
     BlastRadiusEngine,
     IdentityEngine,
